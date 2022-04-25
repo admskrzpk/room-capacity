6d0bf965fd836705352103dafb5c6dc734170511
admskrzpk <100923177+admskrzpk@users.noreply.github.com>
2022-04-25 09:26:14 +0200

    updated test

--- src/test/scala/RoomSpec.scala
+++ src/test/scala/RoomSpec.scala
@@ -1,5 +1,6 @@
 import org.scalatest.flatspec._
 import org.scalatest.matchers._
+
 class RoomSpec extends AnyFlatSpec with should.Matchers {
   behavior of "Room"
   it should "print information if number of people who wants to enter the room is bigger then capacity" in {
@@ -10,14 +11,11 @@ class RoomSpec extends AnyFlatSpec with should.Matchers {
     val expected = println("not enough space")
     actual shouldBe expected
   }
-  it should "return  information with number of people who wants to entered the room in" {
-    val pplEntering = 10
-    val capacity = 20
-    val room2 = Room("Second room", capacity)
-    val actual = room2.enter(pplEntering)
-    val expected = room2
-    actual shouldBe expected
+  it should "return number of ppl who entered the room" in {
+  val secondRoom = Room("Second Room", 15)
+  val pplEntering = 10
+  val actual = secondRoom.enter(pplEntering)
+    val expeced = pplEntering
+    actual shouldBe expeced
   }
-
 }
-
