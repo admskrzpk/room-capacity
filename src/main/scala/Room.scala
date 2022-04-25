bf3e642d01544930ebcca918d313c0a165cfb125
admskrzpk <100923177+admskrzpk@users.noreply.github.com>
2022-04-25 09:48:58 +0200

    updated

--- src/main/scala/Room.scala
+++ src/main/scala/Room.scala
@@ -1,23 +1,15 @@
-case class Room (roomName: String, capacity: Int, var occupied: Int) {
+case class Room(roomName: String, capacity: Int, var occupied: Int) {
 
-def enter(pplEntering: Int) = {
-  if (pplEntering + occupied > capacity) println("not enough space")
-  else pplEntering + occupied
-}
+  def enter(pplEntering: Int) = {
+    if (pplEntering + occupied > capacity) println("not enough space")
+    else pplEntering + occupied
+  }
+
   def leave(pplLeaving: Int) = occupied - pplLeaving
-  def report(pplInside:Int) = println(s"there is $pplInside people inside a room $roomName")
 
+  def report(pplInside: Int) = println(s"there is $pplInside people inside a room $roomName")
 }
 
 object Room extends App {
   val firstRoom = Room("First Room", 20, 0)
 }
-
-
-//Define a Room class that should do the following:
-//
-//A room has a name and capacity
-//capacity = the maximum number of people that can enter a room
-//A person can enter a room (only when there is still free space)
-//A person can leave a room
-//A room can report the number of people inside (who entered)
