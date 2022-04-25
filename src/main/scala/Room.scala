06fa6a92bb04129e81ef9a5dc84dc53f04176cc8
admskrzpk <100923177+admskrzpk@users.noreply.github.com>
2022-04-25 09:48:29 +0200

    updated test

--- src/main/scala/Room.scala
+++ src/main/scala/Room.scala
@@ -1,16 +1,16 @@
-case class Room (roomName: String, capacity: Int) {
+case class Room (roomName: String, capacity: Int, var occupied: Int) {
 
 def enter(pplEntering: Int) = {
-  if (pplEntering > capacity) println("not enough space")
-  else pplEntering
+  if (pplEntering + occupied > capacity) println("not enough space")
+  else pplEntering + occupied
 }
-  def leave(pplLeaving: Int) = ???
+  def leave(pplLeaving: Int) = occupied - pplLeaving
   def report(pplInside:Int) = println(s"there is $pplInside people inside a room $roomName")
 
 }
 
 object Room extends App {
-  val firstRoom = Room("First Room", 20)
+  val firstRoom = Room("First Room", 20, 0)
 }
 
 
